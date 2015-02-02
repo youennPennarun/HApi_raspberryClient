e51ddf9b7523963e1db8b9fcdcb75890cd02b859
Youenn PENNARUN <youenn.pennarun@gmail.com>
2015-02-02 21:03:40 +0100

    solved some bugs

--- hapi/__main__.py
+++ hapi/__main__.py
@@ -15,38 +15,37 @@ from player import Player
 from ConfigParser import SafeConfigParser
 
 def main():
-    config = SafeConfigParser()
-    config.read(expanduser("~") + '/.hapi_conf')
-    scheduler = Sched()
-    
-    def setAlarmData(data):
-        print(str(data))
-    print("starting node js")
-    serverHandler = ServerHandler()
-    
-    serverHandler.on(OnHandler("alarm:new", Alarm.responseToObject))
-    serverHandler.on(OnHandler("response:alarm:get", Alarm.responseToObject))
-    
-    serverHandler.start()
-    Alarm.serverHandler = serverHandler
-    
-    player = Player(serverHandler, config)
-    serverHandler.on(OnHandler("sound:play", player.play))
-    serverHandler.on(OnHandler("sound:resume", player.resume))
-    serverHandler.on(OnHandler("sound:pause", player.pause))
-    serverHandler.on(OnHandler("sound:next", player.next))
-    serverHandler.on(OnHandler("sound:playing:get", player.getPlayingTrack))
-    serverHandler.on(OnHandler("sound:previous", player.previous))
-    serverHandler.on(OnHandler("sound:volume:get", player.getVolume))
-    serverHandler.on(OnHandler("sound:volume:set", player.setVolume))
-    serverHandler.on(OnHandler("alarm:remove", Alarm.removeByData))
-    serverHandler.on(OnHandler("alarm:update", Alarm.update))
-    
-    dataUtils = DataUtils(serverHandler, config)
-    dataUtils.start()
-    
-    
+
     try:
+        config = SafeConfigParser()
+        config.read(expanduser("~") + '/.hapi_conf')
+        scheduler = Sched()
+        
+        def setAlarmData(data):
+            print(str(data))
+        print("starting node js")
+        serverHandler = ServerHandler()
+        
+        serverHandler.on(OnHandler("alarm:new", Alarm.responseToObject))
+        serverHandler.on(OnHandler("response:alarm:get", Alarm.responseToObject))
+        
+        serverHandler.start()
+        Alarm.serverHandler = serverHandler
+        
+        player = Player(serverHandler, config)
+        serverHandler.on(OnHandler("sound:play", player.play))
+        serverHandler.on(OnHandler("sound:resume", player.resume))
+        serverHandler.on(OnHandler("sound:pause", player.pause))
+        serverHandler.on(OnHandler("sound:next", player.next))
+        serverHandler.on(OnHandler("sound:playing:get", player.getPlayingTrack))
+        serverHandler.on(OnHandler("sound:previous", player.previous))
+        serverHandler.on(OnHandler("sound:volume:get", player.getVolume))
+        serverHandler.on(OnHandler("sound:volume:set", player.setVolume))
+        serverHandler.on(OnHandler("alarm:remove", Alarm.removeByData))
+        serverHandler.on(OnHandler("alarm:update", Alarm.update))
+        
+        dataUtils = DataUtils(serverHandler, config)
+        dataUtils.start()
         while True:
               time.sleep(0.2)
     except KeyboardInterrupt:
